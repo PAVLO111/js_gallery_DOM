caf2ecd7d16862d2bacb870310aa68d465a5126b
pavlo <nadazasada@gmail.com>
2026-05-18 23:26:05 +0300

    test-versions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// -1-
+
+// const mainImage = document.querySelector('#largeImg');
+
+// const listImg = document.querySelectorAll('.gallery__img');
+
+// listImg.forEach((el) => {
+//   el.addEventListener('click', (event) => {
+//     event.preventDefault();
+
+//     mainImage.src = el.src;
+//   });
+// });
+
+// -2-
+
+const mainImage = document.querySelector('#largeImg');
+
+const list = document.querySelector('#thumbs');
+
+list.addEventListener('click', (event) => {
+  const link = event.target.closest('.list-item__link');
+
+  if (!link) {
+    return;
+  }
+
+  event.preventDefault();
+
+  const miniImg = link.querySelector('img');
+
+  if (miniImg) {
+    mainImage.src = miniImg.src;
+  }
+});
