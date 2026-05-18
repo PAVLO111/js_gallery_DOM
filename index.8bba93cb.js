89427eb76151d9ed2b278209a6b2531adb750af9
pavlo <nadazasada@gmail.com>
2026-05-18 23:38:48 +0300

    make build

---
+++ index.8bba93cb.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){var r=t.target.closest(".list-item__link");r&&(t.preventDefault(),r.querySelector("img")&&(e.src=r.getAttribute("href")))});
+//# sourceMappingURL=index.8bba93cb.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const mainImage = document.querySelector("#largeImg");
+const list = document.querySelector("#thumbs");
+list.addEventListener("click", (event)=>{
+    const link = event.target.closest(".list-item__link");
+    if (!link) return;
+    event.preventDefault();
+    const miniImg = link.querySelector("img");
+    if (miniImg) // mainImage.src = miniImg.src;
+    mainImage.src = link.getAttribute("href");
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
