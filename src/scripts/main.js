8d16324552e0da29c2aa23b5f60851986e6421b0
pavlo <nadazasada@gmail.com>
2026-05-19 18:20:51 +0300

    rename variable

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,19 +4,18 @@ const mainImage = document.querySelector('#largeImg');
 
 const list = document.querySelector('#thumbs');
 
-list.addEventListener('click', (event) => {
-  const link = event.target.closest('.list-item__link');
+list.addEventListener('click', (e) => {
+  const link = e.target.closest('.list-item__link');
 
   if (!link) {
     return;
   }
 
-  event.preventDefault();
+  e.preventDefault();
 
   const miniImg = link.querySelector('img');
 
   if (miniImg) {
-    // mainImage.src = miniImg.src;
     mainImage.src = link.getAttribute('href');
   }
 });
